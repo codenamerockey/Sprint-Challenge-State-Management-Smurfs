64cb8fa1e2311338244788f37df20a3863d2832a
Rockey Simmons <codenamerockey@gmail.com>
2019-09-13 09:26:30 -0700

    added reducer file and did self-study questions

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import React from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { getSmurf } from './action';
 
-export default App;
+const App = ({ state, getSmurf }) => {
+  return (
+    <div className="App">
+      <h1>{state.name}</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>
+        <button onClick={() => getSmurf()}>Get Your Smurf</button>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    state: state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSmurf }
+)(App);
